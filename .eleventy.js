75567b81d556199eb082e794faeee5985911e0cb
Eric <eric.wolter@gmx.de>
2021-04-07 12:36:05 +0200

    minify

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,34 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget('./img');
+  eleventyConfig.addWatchTarget('./js');
   eleventyConfig.addWatchTarget('./css');
   eleventyConfig.addWatchTarget('./data');
 
   eleventyConfig.addPassthroughCopy('./ads.txt');
 
-  eleventyConfig.addPassthroughCopy('./img');
-  eleventyConfig.addPassthroughCopy('./js');
-  eleventyConfig.addPassthroughCopy('./css');
   eleventyConfig.addPassthroughCopy('./data');
+
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    console.log(outputPath);
+    if( outputPath && outputPath.endsWith(".html") ) {
+      console.log(outputPath);
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+      console.log(content);
+      console.log(minified);
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
